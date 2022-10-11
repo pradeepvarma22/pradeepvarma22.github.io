84ba34bdd796491560cc71aa949ec1737a0cc415
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-10-11 10:27:41 +0530

    init
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@
 import profile from '../assets/profile.png';
 import resumePdf from '../assets/resume.pdf';
 
-
 const Header = () => {
     return (
         <section className='header'>
@@ -13,8 +12,7 @@ const Header = () => {
                 <p>Full Stack Blockchain Developer</p>
                 <div>
                     <div className='p-5 space-x-2'>
-
-                        <a href='mailto:pradeepvarma2292@example.com' className='w-full py-3.5 text-xl text-center text-white transition-colors duration-300 bg-blue-400 rounded-md hover:bg-blue-500 ease px-9 md:w-auto'>Email</a>
+                        <a href='mailto:pradeepvarma2292@gmail.com' className='w-full py-3.5 text-xl text-center text-white transition-colors duration-300 bg-blue-400 rounded-md hover:bg-blue-500 ease px-9 md:w-auto'>Email</a>
                         <a href='https://github.com/pradeepvarma22' className='w-full py-3.5 text-xl text-center text-white transition-colors duration-300 bg-blue-400 rounded-md hover:bg-blue-500 ease px-9 md:w-auto'>Github</a>
                     </div>
                     <div>
